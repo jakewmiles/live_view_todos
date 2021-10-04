ee3fee255ad38d23896c9fc9a07625a2b16e066b
jakewmiles <jake.w.miles@gmail.com>
2021-10-04 08:47:41 +0100

    feat: add .html.heex for rendering

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -1,8 +1,9 @@
 defmodule LiveViewTodosWeb.TodoLive do
   use LiveViewTodosWeb, :live_view
+  alias LiveViewTodos.Todos
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, todos: Todos.list_todos())}
   end
 
   def render(assigns) do
